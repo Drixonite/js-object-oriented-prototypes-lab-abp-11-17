ab89adc240fe2c6dbcd9864fe762f34a0dce9813
David Kim <mail.ddk@gmail.com>
2017-12-02 23:31:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
-  this.prototype.veto = function(){
-    return "No, I must disagree"
-  }
 }
 
 
+BoardMember.prototype.veto = function(){
+  return "No, I must disagree"
+}
   this.prototype.approve = function(){
     return 'You can do that!'
   }
