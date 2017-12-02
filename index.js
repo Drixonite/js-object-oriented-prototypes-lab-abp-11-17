50cb63d00c325e47c7174ae5f06623079edd36eb
David Kim <mail.ddk@gmail.com>
2017-12-02 23:29:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
-}
-
   this.prototype.veto = function(){
     return "No, I must disagree"
   }
+}
+
+
   this.prototype.approve = function(){
     return 'You can do that!'
   }
@@ -17,7 +18,6 @@ function BoardMember(name, homeState, training){
   this.prototype.releasePressStatement = function(){
     return "You will see great things from Scuber."
   }
-
   this.prototype.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
